14c3c310b0846e127e07fdb794a763f4d7714105
Aditya Gummididala <adityagvssss@gmail.com>
2022-12-18 22:04:18 +0530

    Update Main.java

--- src/temperature/Main.java
+++ src/temperature/Main.java
@@ -1,10 +1,14 @@
 package temperature;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+      Scanner input = new Scanner(System.in);
+      System.out.println("enter the temp in Celcius");
+      float f = input.nextFloat();
+      double d = (f*1.8)+32;
+      System.out.println("temp in farenheit is "+d);
 	}
 
 }
